f3371306d61c603a6db7ec5fe4f3a6931fae34fe
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-10-26 23:02:04 +0800

    Konversi String ke Tipe Data Primitif

---
+++ Day18.java
@@ -0,0 +1,7 @@
+public class Day18 {
+    public static void main(String[] args){
+        String str = "123";
+int angka = Integer.parseInt(str); // mengonversi string menjadi int
+System.out.println(str);
+    }
+}
