13dc9efb6fd22a47ada3bf3a10f05b17d26655bd
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-10-28 21:24:27 +0800

    Konversi Tipe Data Primitif ke String

---
+++ Day20.java
@@ -0,0 +1,7 @@
+public class Day20 {
+    public static void main(String[] args){
+        int angka2 = 456;
+        String str2 = Integer.toString(angka2); // mengubah int menjadi string
+        System.out.println(str2);
+    }
+}
