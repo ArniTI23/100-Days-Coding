0fe8eb9125b4712bf3125e42ba2f6e95e8360cd5
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-10-31 23:52:58 +0800

    Inisialisasi variabel

---
+++ Day23.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    public static void main(String[] args) {
+   
+      // Deklarasi variabel
+      int nilaiInt;
+      double sisiLebar;
+      boolean nilaiBenar;
+      char nilaiKarakter;
+	  
+      // Memberi nilai ke variabel
+      nilaiInt = 10;
+      sisiLebar = 5.4;
+      nilaiBenar = true;
+      nilaiKarakter = 'B';
