1a26e616f587ec803a9f70f08e29a5e00f4a0c9c
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-01 22:35:56 +0800

    update variabel

---
+++ Day24.java
@@ -0,0 +1,32 @@
+public class Main {
+    
+    public static void main(String[] args) {
+   
+      // Deklarasi variabel
+      int nilaiInt;
+      double sisiLebar;
+      boolean nilaiBenar;
+      char nilaiKarakter;
+	  
+      // Memberi nilai ke variabel
+      nilaiInt = 10;
+      sisiLebar = 5.4;
+      nilaiBenar = true;
+      nilaiKarakter = 'B';
+        
+        //update variabel
+        nilaiInt = 20;
+      sisiLebar = 5.5;
+      nilaiBenar = false;
+      nilaiKarakter = 'A';
+	  
+      // Menampilkan hasil
+      System.out.println();
+      System.out.println("nilaiInt = " + nilaiInt);
+      System.out.println("sisiLebar = " + sisiLebar);
+      System.out.println("nilaiBenar = " + nilaiBenar);
+      System.out.println("nilaiKarakter = " + nilaiKarakter);
+   }
+    
+        
+                         }
