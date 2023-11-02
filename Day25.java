1082fd90154f09bf22fc813cf2eb0c06df6bf402
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-02 20:06:26 +0800

    Constant (final variable)

---
+++ Day25.java
@@ -0,0 +1,12 @@
+public class Main {
+   public static void main(String args[]) {
+       class Data {
+   final int integerConstant = 20;
+}
+      Data obj1 = new Data();
+      System.out.println("nilai bilangan bulat Konstanta: "+obj1.integerConstant);
+      Data obj2 = new Data();
+      System.out.println("nilai bilangan bulat Konstanta: "+obj2.integerConstant);
+   }
+}
+
