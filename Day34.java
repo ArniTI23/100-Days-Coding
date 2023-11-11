e2ea223efffe8d8d61763809329b1914675bc6e6
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-11 21:47:55 +0800

    Ternary Operator

---
+++ Day34.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class ternary {
+     public static void main(String[] args){
+         int n1 = 5, n2 = 10, hasil;
+         
+         System.out.println("Angka pertama : " + n1);
+         System.out.println("Angka kedua   : " + n2);
+        
+         hasil= (n1 < n2) ? (n1 + n2) : (n1 - n2);
+         System.out.println("Maksimalnya adalah = " + hasil);
+
+     
+ }
+          
+}
