acd308e72cf1780354157088c6f843ab139e74b7
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-14 20:52:02 +0800

    Operator penugasan

---
+++ Day37.java
@@ -0,0 +1,17 @@
+public class day37 {
+    
+    public static void main(String[] args) {
+        
+        int angka1 = 5;
+        int angka2 = 10;
+
+        angka1 += 3; 
+        System.out.println("Hasil penugasan +=: " + angka1);
+        
+        angka2 *= 2; 
+        System.out.println("Hasil penugasan *=: " + angka2);
+
+        angka2 /= 4; 
+        System.out.println("Hasil penugasan /=: " + angka2);
+    }
+}
