5f5ce9673eb977dad9c41eaae7dcc6783e21e3b5
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-15 20:24:24 +0800

    Operator Perbandingan

---
+++ Day38.java
@@ -0,0 +1,27 @@
+public class day38 {
+   public static void main(String[] args) {
+        int A = 12;
+        int B = 4;
+        boolean hasil;
+       
+        hasil = A > B;
+        System.out.println(hasil);
+       
+        hasil = A < B;
+        System.out.println(hasil);
+        
+        hasil = A >= B;
+        System.out.println(hasil);
+       
+        hasil = A <= B;
+        System.out.println(hasil);
+        
+        hasil = A == B;
+        System.out.println(hasil);
+
+        hasil = A != B;
+        System.out.println(hasil);
+
+    }
+ 
+}
