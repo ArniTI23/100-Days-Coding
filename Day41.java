6f3afc978b67cb982b6bd1491edc550a8800b87e
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-18 20:54:04 +0800

    Perulangan do while

---
+++ Day41.java
@@ -0,0 +1,9 @@
+public class day41 {
+    public static void main(String[] args){
+      int a=0;
+        do{
+            System.out.print(a+" ");
+            a++;
+        }while(a<=10);
+  }
+}
