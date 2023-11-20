1ff0f55778bdd286d5537543238faf97e6853566
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-20 23:32:50 +0800

    Impor Output while

---
+++ Day43.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class soal2 {
+    public static void main(String[] args){
+        Scanner in= new Scanner(System.in);
+        
+        
+        while(true) {
+            System.out.print("masukkan nama anda :");
+            String nama=in.nextLine();
+            if(nama.equalsIgnoreCase("1arni")){
+                break;
+            }
+            
+        }
+        
+        
+        }
+    }
+        
+    
