e6affe59a06d2945f7cf70ed1a34e25e6ddf240c
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-24 23:53:09 +0800

    Perulangan break

---
+++ Day46.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class day46 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                System.out.println(i);
+                break;
+            }
+            System.out.println(i);
+        }
+        }
+}
