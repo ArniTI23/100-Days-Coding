341d64f4974e7e3b34b16fe5d1b392dd79220ae0
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-24 23:56:38 +0800

    Perulangan continue

---
+++ Day47.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class day47 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                System.out.println("angka"+i);
+                continue;
+            }
+            System.out.println("angka"+i);
+        }
+    }
+}
+
