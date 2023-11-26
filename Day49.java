7c07794dd7237ee71a28d12e8589ecfe2ddab5e7
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-26 22:07:56 +0800

    Belajar menggunakan array

---
+++ Day49.java
@@ -0,0 +1,9 @@
+public class day49 {
+    public static void main(String[] args){
+        String buah[]={"apel","anggur","mangga"};
+        for(int i=0; i<buah.length; i++){
+            System.out.println("buah ke-"+i+": "+buah[i]);
+        }
+        
+    }
+}
