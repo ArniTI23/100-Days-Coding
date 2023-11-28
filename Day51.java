c4abae30f06e279ea9204fd2fbeeeee8c8a0cc23
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-28 22:26:27 +0800

    Array 1D

---
+++ Day51.java
@@ -0,0 +1,19 @@
+public class yeh {
+    public static void main(String[] args) {
+        int[] angka = new int[5];
+
+        angka[0] = 10;
+        angka[1] = 20;
+        angka[2] = 30;
+        angka[3] = 40;
+        angka[4] = 50;
+        
+        System.out.println("Isi array:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        
+        }
+    }
+
+
+}
