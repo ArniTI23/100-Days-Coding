2ae67eca6626295e3dbac82be0b4665a4628fe25
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-11-29 23:28:03 +0800

    Menjumlahkan isi array

---
+++ Day52.java
@@ -0,0 +1,10 @@
+public class day52 {
+    public static void main(String[] args) {
+    int nilai[]={2,5,10};
+        int jumlah=0;
+    for(int i=0; i<nilai.length; i++){
+        jumlah +=nilai[i];
+    }
+        System.out.println(jumlah);
+}
+}
