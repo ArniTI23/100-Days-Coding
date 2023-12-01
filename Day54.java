87afcde1f3c2d72932ea3e85a9d0aa1a9716cff0
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-01 22:23:18 +0800

    Deklarasi Array bertipe non-primitif

---
+++ Day54.java
@@ -0,0 +1,8 @@
+public class day54 {
+    public static void main(String[] args) {
+        String[] teman = {"arni","elma","seli"};
+        for (int i = 0; i < teman.length; i++) {
+            System.out.println(teman[i]);
+        }
+    }
+}
