97e90757917548f823c07c2d46dbc558321b0c37
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-02 17:22:40 +0800

    Mencetak nilai array dengan Do-while

---
+++ Day55.java
@@ -0,0 +1,10 @@
+public class day55 {
+    public static void main(String[] args){
+        int nilai[]={10,20,30,40,50};
+        int i=0;
+        do{
+            System.out.println(nilai[i]);
+            i++;
+        }while(i<nilai.length);
+    }
+}
