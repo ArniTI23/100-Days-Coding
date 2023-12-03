7b0eab2f69b1f905ec329f6be4f67131ecaf7c60
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-03 21:59:42 +0800

    Mencetak isi array dengan while

---
+++ Day56.java
@@ -0,0 +1,11 @@
+public class daylimaenam {
+    public static void main(String[] args){
+        int nilai[]={1,2,3,4,5};
+        int i=0;
+        while(i<nilai.length){
+            System.out.print(nilai[i]);
+            i++;
+        }
+        
+    }
+}
