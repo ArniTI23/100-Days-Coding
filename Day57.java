260b47c770500b305a780f946ffd5186f9f0ed63
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-04 22:25:20 +0800

    Mengambil Data dari Array

---
+++ Day57.java
@@ -0,0 +1,7 @@
+public class day57 {
+    public static void main(String[] args){
+        String teman[]={"tika","Arni","seli","elma","yitri"};
+        System.out.println(teman[1]);
+        
+    }
+}
