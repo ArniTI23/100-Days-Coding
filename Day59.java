41da9d47769c6ebd0898017891b45f67ceab5543
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-06 21:26:56 +0800

    Mencetak nilai array dengan for

---
+++ Day59.java
@@ -0,0 +1,11 @@
+public class day59 {
+    public static void main(String[] args){
+        int [][] data={{10,20,30},{50,60,70}};
+        
+        for(int i=0; i<data.length; i++){
+            for(int j=0; j<data[i].length; j++){
+                System.out.println(data[i][j]+ "");
+            }
+        }
+    }
+}
