6ebdbba5256b44e3e8a25286425d47c8cc618582
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-07 23:28:48 +0800

    Mencetak isi array dengan for-each

---
+++ Day60.java
@@ -0,0 +1,14 @@
+public class day60 {
+    public static void main(String[] args){
+        int [][] data={
+            {2,4,6,8},
+            {3,5,7,9}
+        };
+        for(int[] baris:data){
+            for(int elemen:baris){
+                System.out.println(elemen+" ");
+            }
+        }
+        
+    }
+}
