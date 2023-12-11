555a92198d14a4c57eaf396726a2f48668c04829
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-11 22:06:38 +0800

    Mencetak isi array 2D dengan menggunakan while

---
+++ Day64.java
@@ -0,0 +1,18 @@
+public class day4 {
+    public static void main(String[] args){
+        int [][] data={
+            {1,2,3,4},
+            {5,6,7,8}
+        };
+        int i=0;
+        while(i<data.length){
+            int j=0;
+            while (j<data[i].length){
+                System.out.print(data[i][j] + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
