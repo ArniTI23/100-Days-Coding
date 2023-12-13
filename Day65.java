131919d777a3a1ed3d2b673fb5c45be6fe55a1a2
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-13 18:29:42 +0800

    Array add item

---
+++ Day65.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class day65 {
+    public static void main(String[] args){
+        ArrayList data=new ArrayList<>();
+        data.add(1);
+        data.add("arni");
+        data.add('A');
+        data.add(148.2);
+        
+        System.out.println(data);
+    }
+}
