f1553d123eef8dcfd20e8d47bd90581254632534
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-14 23:32:54 +0800

    Mencetak isi array dengan kelas arrays

---
+++ Day67.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+public class day67 {
+    public static void main(String[] args){
+        int[] nilai={10,20,30,40};
+        System.out.print(Arrays.toString(nilai));
+    }
+}
