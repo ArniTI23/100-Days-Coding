aa2438db7623529e7cf73d92cb5ce761551a6eeb
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-15 22:57:04 +0800

    Pencetak isi array 2D dengan kelas Arrays

---
+++ Day68.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class day68 {
+    public static void main(String[] args){
+        int [][] data={
+            {1,2,3,4},
+            {5,6,7,8}
+       };
+        System.out.println(Arrays.deepToString(data));
+    }
+
+}
