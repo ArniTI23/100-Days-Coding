f324c8404d061ad71cf717c41a508697e786f827
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-18 23:10:06 +0800

    Contoh looping while

---
+++ Day71.java
@@ -0,0 +1,14 @@
+public class day71 {
+    public static void main(String[] args){
+        int angka=1;
+        while(angka<10){
+            System.out.println(angka);
+            angka++;
+        }
+        int nilai=10;
+        while(nilai>0){
+            System.out.println(nilai);
+            nilai -=1;
+        }
+    }
+}
