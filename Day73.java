58c19a7c71bddb9e0f3603c09bc08203513a5d8e
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-20 22:54:00 +0800

    Operasi transpose matriks menggunakan Array 2D

---
+++ Day73.java
@@ -0,0 +1,36 @@
+public class day72 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] transpose = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("Matriks Awal:");
+        displayMatrix(matrix);
+
+        System.out.println("\nMatriks Transpose:");
+        displayMatrix(transpose);
+    }
+
+    static void displayMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
