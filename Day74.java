9ac6107f1b6dfca46c4eb49295b8c1cba74f341b
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-21 20:51:00 +0800

    Operasi perkalian matriks menggunakan Array 2D

---
+++ Day74.java
@@ -0,0 +1,35 @@
+public class day74 {
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix2 = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+
+        int rowsMatrix1 = matrix1.length;
+        int colsMatrix1 = matrix1[0].length;
+        int rowsMatrix2 = matrix2.length;
+        int colsMatrix2 = matrix2[0].length;
+
+        if (colsMatrix1 != rowsMatrix2) {
+            System.out.println("Operasi perkalian matriks tidak dapat dilakukan.");
+            return;
+        }
+
+        int[][] resultMatrix = new int[rowsMatrix1][colsMatrix2];
+
+        for (int i = 0; i < rowsMatrix1; i++) {
+            for (int j = 0; j < colsMatrix2; j++) {
+                for (int k = 0; k < colsMatrix1; k++) {
+                    resultMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        System.out.println("Hasil perkalian matriks:");
+        for (int i = 0; i < rowsMatrix1; i++) {
+            for (int j = 0; j < colsMatrix2; j++) {
+                System.out.print(resultMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
