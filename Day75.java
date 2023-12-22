dfa70095abb3758e395dc27315823fc9f3d4063f
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-22 22:57:18 +0800

    Membuat Fungsi

---
+++ Day75.java
@@ -0,0 +1,14 @@
+public class day75 {
+    static int hitung(int input){
+        int hasil;
+        hasil=input*input;
+        return hasil;
+    }
+    public static void main(String[] args){
+        int y,x;
+        x=5;
+        y=hitung (x);
+        System.out.println("x ="+x+"\ny ="+y);
+        
+    }
+}
