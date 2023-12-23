11991dfc34efa00b9d37c539f9768e1ad31d329f
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-23 20:34:40 +0800

    Method untuk menjumlahkan dua angka

---
+++ Day76.java
@@ -0,0 +1,9 @@
+public class day76 {
+    public static int tambah(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        int hasil=tambah(5,3);
+        System.out.println("hasil penjumlahan: "+hasil);
+    }
+}
