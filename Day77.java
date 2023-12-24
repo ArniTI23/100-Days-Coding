57f5e124e37c2883fbb022278043d36b8820e290
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-24 20:54:10 +0800

    Contoh membuat method untuk menyambung kata

---
+++ Day77.java
@@ -0,0 +1,9 @@
+public class day77 {
+    public static String sambungKata(String kata1, String kata2) {
+        return kata1 + " " + kata2;
+    }
+    public static void main(String[] args) {
+    String hasil = sambungKata("Halo", "Arni");
+    System.out.println("Hasil penggabungan kata: " + hasil);
+    }
+}
