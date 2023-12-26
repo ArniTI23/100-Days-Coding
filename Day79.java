c3ca50f3552c2d932a4551486d3ad029d47c37a1
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-26 21:29:21 +0800

    Contoh penggunaan continue untuk dua angka dalam loop for

---
+++ Day79.java
@@ -0,0 +1,10 @@
+public class day79 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            if(i==4 || i==8){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
