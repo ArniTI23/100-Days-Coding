1bc8e201d6fbbc63d863c771a66589f27eaeb048
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-27 21:26:04 +0800

    Contoh continue dalam loop while

---
+++ Day80.java
@@ -0,0 +1,12 @@
+public class day80 {
+    public static void main(String[] args){
+        int i=0;
+        while(i<5){
+            i++;
+            if(i==2){
+                continue;
+            }
+            System.out.println("angka ke-"+i);
+        }
+    }
+}
