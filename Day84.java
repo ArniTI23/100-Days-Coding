2e5d751596ee268af34791fd416576baf5b417b6
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2023-12-31 21:33:02 +0800

    Fungsi static

---
+++ Day84.java
@@ -0,0 +1,10 @@
+public class day84 {
+    public static int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int hasil = day84.tambah(5, 5);
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
