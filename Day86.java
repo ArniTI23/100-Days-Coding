a25b2b8ac9b2d7203b38718c38e83f6beac620ab
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-02 19:43:45 +0800

    Fungsi void

---
+++ Day86.java
@@ -0,0 +1,8 @@
+public class day86 {
+    public static void sapaUser(String nama) {
+        System.out.println("Halo, " + nama + "!");
+    }
+    public static void main(String[] args) {
+        sapaUser("Arni"); 
+    }
+}
