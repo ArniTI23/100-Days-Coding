eaff6a16f1ebaf17830f47bf1c465e6a5f1ee2c5
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-05 19:22:06 +0800

    String charAt( )

---
+++ Day89.java
@@ -0,0 +1,10 @@
+public class day89 {
+    public static void main(String[] args){
+        String contoh="hello world";
+        char a=contoh.charAt(4);
+        char b=contoh.charAt(8);
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
