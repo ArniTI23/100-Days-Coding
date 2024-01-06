aa9f4f9ef12891af00a8a9e5db454d4206bdfccb
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-06 21:14:35 +0800

    String charAt( )

---
+++ Day90.java
@@ -0,0 +1,14 @@
+public class day90 {
+    public static void main(String [] args){
+        String nama= "Arni";
+        char huruf1=nama.charAt(0);
+        char huruf2=nama.charAt(1);
+        char huruf3=nama.charAt(2);
+        char huruf4=nama.charAt(3);
+        
+        System.out.print(huruf1);
+        System.out.print(huruf2);
+        System.out.print(huruf3);
+        System.out.print(huruf4);
+    }
+}
