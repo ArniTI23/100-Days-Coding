59cd07b07babbc867420a3b2fc86fc7c6d58d848
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-07 20:21:11 +0800

    length( )/size array

---
+++ Day91.java
@@ -0,0 +1,9 @@
+public class day91 {
+    public static void main(String [] args){
+        int[] angka = {1, 2, 3, 4, 5};
+        int panjangArray = angka.length;
+        
+        System.out.println(panjangArray);
+
+    }
+}
