03f1b2b18863437f9c761d1efdb6bbd3e6114ff4
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-08 19:23:32 +0800

    toLowerCase( )

---
+++ Day92.java
@@ -0,0 +1,9 @@
+public class toLowerCase {
+    public static void main(String [] args){
+        String a = "CONTOH";
+        String b= a.toLowerCase();
+
+        System.out.println(b);
+
+    }
+}
