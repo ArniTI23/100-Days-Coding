c7f6aead3cc7609f2bbd9b095217ca603ff3236d
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-09 20:45:29 +0800

    toUpperCase( )

---
+++ Day93.java
@@ -0,0 +1,8 @@
+public class day93 {
+    public static void main(String [] args){
+        String kata= "contoh";
+        String hasil=kata.toUpperCase();
+        
+        System.out.println(hasil);
+    }
+}
