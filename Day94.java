8b6cbaf46a1476286e50198678aeea943054d8f5
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-10 19:14:18 +0800

    Contoh penggunaan Contains

---
+++ Day94.java
@@ -0,0 +1,8 @@
+public class day94 {
+    public static void main(String[] args){
+        String kalimat="ini adalah contoh penggunaan metode contains";
+        boolean hasil=kalimat.contains("contoh");
+        
+        System.out.println("Apakah kalimat mengandungkata contoh?"+"\n="+hasil);
+    }
+}
