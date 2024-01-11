1f03e54419af29c4591059facda4eb90aa042241
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-11 18:55:26 +0800

    Contains

---
+++ Day95.java
@@ -0,0 +1,10 @@
+public class day95 {
+    public static void main(String[] args){
+        String kalimat="Halo, apa kabar?";
+        if(kalimat.contains("apa")){
+            System.out.println("kalimat tersebut mengandung kata'apa'.");
+        }else{
+            System.out.println("kalimat tersebut tidak mengandung kata 'apa'.");
+        }
+    }
+}
