1060080d02f364ba4405cead09bbb6ced3dcd148
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-13 19:50:01 +0800

    String Concatenation

---
+++ Day97.java
@@ -0,0 +1,8 @@
+public class day97 {
+    public static void main(String[] args){
+        String a="Hello";
+        String b="World";
+        String c=a.concat(" ").concat(b);
+        System.out.println(c);
+    }
+}
