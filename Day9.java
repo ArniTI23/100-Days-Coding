5ab69f257101499e57ba754c63f16240e0a8c4fd
ArniTI23 <147065111+ArniTI23@users.noreply.github.com>
2024-01-15 19:02:24 +0800

    String equals( )

---
+++ Day9.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class day99 {
+    public static void main(String[] args){
+        Scanner in= new Scanner(System.in);
+        System.out.print("Masukkan nama anda :");
+        String a=in.nextLine();
+        
+        if(a.equals("arni")){
+            System.out.println("benar");
+        }else{
+            System.out.println("salah");
+        }
+        
+    }
+}
